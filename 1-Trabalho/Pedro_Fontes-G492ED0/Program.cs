63d532ac2d4218bff5b7e0f5fcc44ad304849657
Pedro Henrique Fontes Silva <142354849+PedroHTFSilva@users.noreply.github.com>
2023-09-16 16:25:04 -0300

    1-Trabalho

---
+++ 1-Trabalho/Pedro_Fontes-G492ED0/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Primeirotrabalho;
+
+namespace Primeirotrabalho
+{
+    class Program    
+    {
+        static void Main (string[] args)
+        {
+    
+        Aluno aluno = new Aluno();   
+        aluno.ExibirNome();
+        aluno.ExibirIdade();
+        aluno.ExibirRa();
+        aluno.ExibirCurso();
+    
+        }
+    }
+
+   
+}
+ public class Pessoa 
+    {
+       public  int idade = 22;
+       public string nome = "Pedro Henrique Fontes";
+
+       public void ExibirIdade()
+       {
+        Console.WriteLine("A idade do aluno é: " + idade);
+       }   
+       public void ExibirNome()
+       {
+        Console.WriteLine("O nome do aluno é: " + nome);
+       }
+
+        }
+
+        public class Aluno : Pessoa 
+    { 
+        public string ra = "G492ED-0";
+        public string curso ="Análise e Desenvolvimento de Sistemas.";
+        public void ExibirRa() 
+        {
+            Console.WriteLine($"O RA do aluno é: " + ra);
+        }
+
+        public void ExibirCurso() 
+        {
+            Console.WriteLine("Aluno faz: "+ curso);
+        }
+    }
+   
